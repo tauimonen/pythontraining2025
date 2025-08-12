efce03c60a2c674baa971701cbec35e5c7767ea2
tauimonen <tauimonen@gmail.com>
2025-08-12 14:37:28 +0300

    Create buy_computer

--- buy_computer.py
+++ buy_computer.py
@@ -1,1 +1,36 @@
-print("HOLA")
+available_parts = [
+    "computer",
+    "monitor",
+    "keyboard",
+    "mouse",
+    "mouse mat",
+    "hdmi cable",
+    "dvd drive"
+]
+current_choice = "-"
+computer_parts = []
+
+print("HOLA customer!")
+while current_choice != "0":
+    if current_choice in "123456":
+        print(f"Adding: {current_choice}")
+        if current_choice == "1":
+            computer_parts.append("computer")
+        elif current_choice == "2":
+            computer_parts.append("monitor")
+        elif current_choice == "3":
+            computer_parts.append("keyboard")
+        elif current_choice == "4":
+            computer_parts.append("mouse")
+        elif current_choice == "5":
+            computer_parts.append("mouse mat")
+        elif current_choice == "6":
+            computer_parts.append("hdmi cable")
+    else:
+        print("Please add options from the list below:")
+        for part in available_parts:
+            print(f"{available_parts.index(part) + 1}: {part}")
+
+    current_choice = input()
+
+    print(computer_parts)
