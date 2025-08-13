c1ec5e05bc0f8c7508c9e11790f7c8bea3403ed3
tauimonen <tauimonen@gmail.com>
2025-08-13 12:10:32 +0300

    Refactor the code

--- buy_computer.py
+++ buy_computer.py
@@ -7,25 +7,22 @@ available_parts = [
     "hdmi cable",
     "dvd drive"
 ]
+#valid_choices = [str(i) for i in range(1, len(available_parts) + 1)]
+valid_choices = []
+for i in range(1, len(available_parts) + 1):
+    valid_choices.append(str(i))
+
+
 current_choice = "-"
 computer_parts = []
 
 print("HOLA customer!")
 while current_choice != "0":
-    if current_choice in "1234567":
+    if current_choice in valid_choices:
         print(f"Adding: {current_choice}")
-        if current_choice == "1":
-            computer_parts.append("computer")
-        elif current_choice == "2":
-            computer_parts.append("monitor")
-        elif current_choice == "3":
-            computer_parts.append("keyboard")
-        elif current_choice == "4":
-            computer_parts.append("mouse")
-        elif current_choice == "5":
-            computer_parts.append("mouse mat")
-        elif current_choice == "6":
-            computer_parts.append("hdmi cable")
+        index = int(current_choice) - 1
+        chosen_part = available_parts[index]
+        computer_parts.append(chosen_part)
     else:
         print("Please add options from the list below:")
         for number, part in enumerate(available_parts):
@@ -33,4 +30,4 @@ while current_choice != "0":
 
     current_choice = input()
 
-    print(computer_parts)
+print(computer_parts)
